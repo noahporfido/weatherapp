fae81c5a4d657dda24e7d4ebfddc671528944c04
Noah Porfido <noah.porfido@icloud.com>
2021-10-18 15:37:02 +0200

    init, base structure

---
+++ src/components/InputField.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="form__group field">
+    <input
+      :type="type"
+      class="form__field"
+      :placeholder="placeholder"
+      id="inputField"
+      :required="required"
+    />
+    <label for="inputField" class="form__label">{{ placeholder }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+      validator: (value) => {
+        return ["number", "checkbox", "text"].indexOf(value) !== -1;
+      },
+    },
+    placeholder: {
+      type: String,
+      validator: (value) => {
+        return value.length < 20;
+      },
+    },
+    required: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form__group {
+  position: relative;
+  padding: 15px 0 0;
+  margin-top: 10px;
+  width: 100%;
+}
+
+.form__field {
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid;
+  outline: 0;
+  font-size: 1.3rem;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ .form__label {
+    font-size: 1.3rem;
+    cursor: text;
+    top: 20px;
+  }
+}
+
+.form__label {
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+}
+
+.form__field:focus {
+  ~ .form__label {
+    position: absolute;
+    top: 0;
+    display: block;
+    transition: 0.2s;
+    font-size: 1rem;
+    font-weight: 700;
+  }
+  padding-bottom: 6px;
+  font-weight: 700;
+  border-width: 3px;
+  border-image-slice: 1;
+}
+/* reset input */
+.form__field {
+  &:required,
+  &:invalid {
+    box-shadow: none;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home-container"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import inputField from "@/components/InputField.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    inputField,
+  },
+};
 </script>
