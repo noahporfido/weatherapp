475c01227a0ee99eca1b93c5936411b3f57f9db2
Noah Porfido <noah.porfido@icloud.com>
2021-10-19 09:18:25 +0200

    removed fa and navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as NavBar} from './NavBar.vue'
-
 export { default as Loader} from './Loader.vue'
 
 export { default as WeatherCard} from './WeatherCard.vue'
--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@/assets/styles/normalize.css'
 import '@/assets/styles/style.css'
-
-library.add(faUserSecret)
-
-Vue.component('fa', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
